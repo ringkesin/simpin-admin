2aee640841a548d24c09be5905018e45f4e0a12e
Vicky Andreas Dinata <vickycowcool@gmail.com>
2025-03-23 11:31:59 +0700

    2025/03/23 - Approval Pinjaman done

--- app/Livewire/Page/Main/Pinjaman/PinjamanShow.php
+++ app/Livewire/Page/Main/Pinjaman/PinjamanShow.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use App\Traits\MyAlert;
 use App\Traits\MyHelpers;
 use App\Models\Main\PinjamanModels;
+use App\Models\Master\StatusPengajuanModels;
 
 class PinjamanShow extends Component
 {
@@ -20,7 +21,11 @@ class PinjamanShow extends Component
     public $menuCode;
 
     public $loadData;
+    public $listStatus;
     public $id;
+    public $ri_jumlah_pinjaman;
+    public $prakiraan_nilai_pasar;
+    public $p_status_pengajuan_id;
 
     public function mount($id) {
         $this->titlePage = 'Detail Pinjaman Anggota';
@@ -37,6 +42,65 @@ class PinjamanShow extends Component
     public function getData($id) {
         $data = PinjamanModels::find($id);
         $this->loadData = $data;
+        $this->ri_jumlah_pinjaman = $this->loadData['ri_jumlah_pinjaman'];
+        $this->prakiraan_nilai_pasar = $this->loadData['prakiraan_nilai_pasar'];
+        $this->p_status_pengajuan_id = $this->loadData['p_status_pengajuan_id'];
+    }
+
+    public function listStatusPinjaman() {
+        $data = StatusPengajuanModels::all();
+        return $data;
+    }
+
+    public function saveApproval() {
+        $validated = $this->validate([
+            'ri_jumlah_pinjaman' => 'required',
+            'prakiraan_nilai_pasar' => 'required',
+            'p_status_pengajuan_id' => 'required'
+        ], [
+            'ri_jumlah_pinjaman' => 'Jumlah Pinjaman yang Disetujui required',
+            'prakiraan_nilai_pasar' => 'Prakiraan Nilai Pasar required',
+            'p_status_pengajuan_id.required' => 'Status Pengajuan required.'
+        ]);
+
+        try {
+            $post = PinjamanModels::where('t_pinjaman_id', $this->id)->update([
+                'ri_jumlah_pinjaman' => $this->ri_jumlah_pinjaman,
+                'prakiraan_nilai_pasar' => $this->prakiraan_nilai_pasar,
+                'p_status_pengajuan_id' => $this->p_status_pengajuan_id,
+            ]);
+
+            if($post) {
+                $redirect = route('main.pinjaman.show', ['id' => $this->id]);
+                $this->sweetalert([
+                    'icon' => 'success',
+                    'confirmButtonText' => 'Okay',
+                    'showCancelButton' => false,
+                    'text' => 'Data Berhasil Disimpan !',
+                    'redirectUrl' => $redirect
+                ]);
+            } else {
+                $this->sweetalert([
+                    'icon' => 'warning',
+                    'confirmButtonText'  => 'Okay',
+                    'showCancelButton' => false,
+                    'text' => 'Data gagal di update, coba kembali.',
+                ]);
+            }
+        } catch (QueryException $e) {
+            $textError = '';
+            if($e->errorInfo[1] == 1062) {
+                $textError = 'Data gagal di update karena duplikat data, coba kembali.';
+            } else {
+                $textError = 'Data gagal di update, coba kembali.';
+            }
+            $this->sweetalert([
+                'icon' => 'error',
+                'confirmButtonText'  => 'Okay',
+                'showCancelButton' => false,
+                'text' => $textError,
+            ]);
+        }
     }
 
     public function render()
--- resources/views/livewire/page/main/pinjaman/pinjaman-show.blade.php
+++ resources/views/livewire/page/main/pinjaman/pinjaman-show.blade.php
@@ -24,6 +24,106 @@
             <x-elements.detail label="Nomor Anggota">{{ $this->setIfNull($loadData['masterAnggota']['nomor_anggota'], '-') }}</x-elements.detail>
             <x-elements.detail label="Nama Anggota">{{ $this->setIfNull($loadData['masterAnggota']['nama'],'-') }}</x-elements.detail>
             <x-elements.detail label="Telp">{{ $this->setIfNull($loadData['masterAnggota']['userId']['mobile'],'-') }}</x-elements.detail>
+            <x-elements.detail label="Alamat">{{ $this->setIfNull($loadData['masterAnggota']['userId']['alamat'],'-') }}</x-elements.detail>
+            <x-elements.detail label="Email">{{ $this->setIfNull($loadData['masterAnggota']['userId']['email'],'-') }}</x-elements.detail>
+            <x-elements.detail label="Telp">{{ $this->setIfNull($loadData['masterAnggota']['userId']['mobile'],'-') }}</x-elements.detail>
+        </div>
+        <div class="mt-5">
+            <x-elements.header-form>Detail Pinjaman</x-header-form>
+            <x-elements.detail label="Jenis Pinjaman">{{ $this->setIfNull($loadData['masterJenisPinjaman']['nama'],'-') }}</x-elements.detail>
+            <x-elements.detail label="RA Pinjaman">{{ $this->setIfNull('Rp. '.$this->toRupiah($loadData['ra_jumlah_pinjaman']),'-') }}</x-elements.detail>
+            <x-elements.detail label="Alamat yang didaftarkan">{{ $this->setIfNull($loadData['alamat'],'-') }}</x-elements.detail>
+            <x-elements.detail label="No. Rekening">{{ $this->setIfNull($loadData['no_rekening'],'-') }}</x-elements.detail>
+            <x-elements.detail label="Nama Bank">{{ $this->setIfNull($loadData['bank'],'-') }}</x-elements.detail>
+        </div>
+        <div class="mt-5">
+            <x-elements.header-form>Dokumen Peminjam</x-header-form>
+            <div class="grid grid-cols-2 gap-4">
+                <div>
+                    <x-elements.detail label="KTP">
+                        @php
+                            $pp = ($loadData['doc_ktp']) ? '/storage/'.$loadData['doc_ktp'] : asset('assets/img/blank-doc.png');
+                        @endphp
+                        <img class='rounded-2xl' src="{{ $pp }}" width="130">
+                    </x-elements.detail>
+                    <x-elements.detail label="Surat Nikah">
+                        @php
+                            $pp = ($loadData['doc_surat_nikah']) ? '/storage/'.$loadData['doc_surat_nikah'] : asset('assets/img/blank-doc.png');
+                        @endphp
+                        <img class='rounded-2xl' src="{{ $pp }}" width="130">
+                    </x-elements.detail>
+                </div>
+                <div>
+                    <x-elements.detail label="Kartu Keluarga">
+                        @php
+                            $pp = ($loadData['doc_kk']) ? '/storage/'.$loadData['doc_kk'] : asset('assets/img/blank-doc.png');
+                        @endphp
+                        <img class='rounded-2xl' src="{{ $pp }}" width="130">
+                    </x-elements.detail>
+                    <x-elements.detail label="Slip Gaji">
+                        @php
+                            $pp = ($loadData['doc_slip_gaji']) ? '/storage/'.$loadData['doc_slip_gaji'] : asset('assets/img/blank-doc.png');
+                        @endphp
+                        <img class='rounded-2xl' src="{{ $pp }}" width="130">
+                    </x-elements.detail>
+                </div>
+            </div>
+        </div>
+        <div class="mt-5">
+            <x-elements.header-form>Approval</x-header-form>
+            <form wire:submit="saveApproval">
+                <x-elements.detail label="Jumlah Pinjaman yang Disetujui">
+                    <div class="flex">
+                        <div class="flex-none w-8">
+                            Rp.
+                        </div>
+                        <div class="flex-initial w-64">
+                            <x-form.input class="w-full" type="number" name="ri_jumlah_pinjaman" wire:model.lazy="ri_jumlah_pinjaman"/>
+                        </div>
+                    </div>
+                </x-elements.detail>
+                <x-elements.detail label="Prakiraan Nilai Pasar">
+                    <div class="flex">
+                        <div class="flex-none w-8">
+                            Rp.
+                        </div>
+                        <div class="flex-initial w-64">
+                            <x-form.input class="w-full" type="number" name="prakiraan_nilai_pasar" wire:model.lazy="prakiraan_nilai_pasar"/>
+                        </div>
+                    </div>
+                </x-elements.detail>
+                <x-elements.detail label="Status Pengajuan">
+                    <x-form.select-single name="p_status_pengajuan_id" wire:model.lazy="p_status_pengajuan_id" class="w-full md:w-72" id="p_status_pengajuan_id">
+                        <option value="">Pilih Prakiraan Nilai Pasar</option>
+                            @foreach ($this->listStatusPinjaman() as $value)
+                                <option value="{{ $value['p_status_pengajuan_id'] }}">{{ $value['nama'] }}</option>
+                            @endforeach
+                    </x-form.select-single>
+                </x-elements.detail>
+                <div class="grid grid-cols-12 gap-10">
+                    <div class="col-span-12">
+                        <x-elements.button-submit wire:loading.attr="disabled" wire:confirm="Are you sure your data is correct?">
+                            <div wire:loading wire:target="saveApproval">
+                                {{-- <svg class="w-5 h-5 mr-3 animate-spin" viewBox="0 0 24 24"> --}}
+                                    {{-- <span class="sr-only">Loading Save Data</span> --}}
+                                {{-- </svg> --}}
+                                <span class="me-1 animate-spin inline-block size-3 border-[2px] border-current border-t-transparent text-white rounded-full" role="status" aria-label="loading">
+                                    <span class="sr-only">Processing.....</span>
+                                </span>
+                                <span class="xs:block">
+                                    Processing
+                                </span>
+                            </div>
+                            <div class='flex gap-x-1' wire:loading.remove wire:target="saveApproval">
+                                <x-lucide-square-pen class="size-5"/>
+                                <span class="xs:block">
+                                    Save data
+                                </span>
+                            </div>
+                        </x-elements.button-submit>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
